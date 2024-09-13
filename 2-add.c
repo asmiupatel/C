bcc01aae0e1dec6daf871248c1658b493c589d80
uncommonshit <liberteed1@gmail.com>
2024-09-13 15:10:41 -0400

    addition

---
+++ 2-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    printf("Enter number a\n");
+    scanf("%d", &a);
+
+    printf("Enter number b\n");
+    scanf("%d", &b);  
+
+    printf("The sum of a and b is %d\n" , a + b);
+    return 0;
+}
