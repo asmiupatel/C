27023e94665a7b137e81de2a0cb32ae146c2ac5e
uncommonshit <liberteed1@gmail.com>
2024-09-16 18:25:21 -0400

    format specifiers and how to use comments

---
+++ 6-format_specifier_comments.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 6;
+    float b = 3.3;
+    printf("The value of a is %d and the value of b is %0.11f\n", a, b);
+    /*
+    %c - character
+    %f - float
+    %d - integer
+    %l - long
+    %lf - double
+    %LF - long double
+    */
+    return 0;
+}
