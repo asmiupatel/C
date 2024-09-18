155991e78006cabb22db0ad694e420574fdf6e66
asmiupatel <liberteed1@gmail.com>
2024-09-18 15:26:18 -0400

    for loop example

---
+++ 15-for.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}
+
+
+
+/*
+    basic syntax:
+
+    for (Expression 1 ; Expression 2 ; Expression 3){
+        /*code to execute* /
+    }
+*/
