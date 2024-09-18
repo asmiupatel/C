d513b503f51986f2f325f9a9c932baf1ea26eb57
asmiupatel <liberteed1@gmail.com>
2024-09-18 14:54:47 -0400

    while loop example

---
+++ 14-while.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i < 43)
+    {
+        printf("%d\n", i);
+        i = i + 1;
+    }
+    
+    return 0;
+}
+
+
+/*
+    while loop syntax:
+
+    while (condition) {
+    /*code to be executed * /
+    }
+*/
